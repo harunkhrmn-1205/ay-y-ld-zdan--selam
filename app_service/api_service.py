49797f6d3c89b9f3b1409b228d1df9dff0596071
harunkhrmn-1205 <harunkhrmn1205@gmail.com>
2026-03-25 13:59:52 +0300

    Update api_service.py

--- app_service/api_service.py
+++ app_service/api_service.py
@@ -7,6 +7,31 @@ app = Flask(__name__)
 CORS(app)
 
 
-DATABASE_URL = so.getenv(
+DATABASE_URL = os.getenv(
 "DATABASE_URL",
-  "
+  " postgresql://harun:RnfFqLC9a4qwh8nkGWxPCfRhZCrCF9Rw@dpg-d6t8raqa214c73ce46c0-a.oregon-postgres.render.com/hello_cloud_2_0xuy "
+)
+
+def connect_db():
+   return psycopg2.connect(DATABASE_URL)
+ @app.route("/ziyaretciler", methods=["GET", "POST"])
+def ziyaretciler():
+  conn = connect_db()
+  cur = conn.cursor()
+  cur.execute("CREATE TABLE IF NOT EXISTS ziyaretciler (id SERIAL PRİMARY KEY, isim TEXT)")
+
+if request.method == "POST":
+  isim = request.json.get("isim")
+  if isim:
+     cur.execute("INSER INTO ziyaretciler (isim) VALUES (%s)",(isim,))
+
+  conn.commit()
+  cur.execute("SELECT isim FROM ziyaretciler ORDER BY id DESC LIMIT 10")
+  isimler = [row[0] for row in cur.fetchall()]
+
+
+cur.close()
+conn.close()
+ return jsonify(isimler)
+if__name__=="__main__":
+ app.run(host="0.0.0.0", port=5001)
