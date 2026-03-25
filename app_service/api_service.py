d214da5b85735763d196bd8604343901ca179461
harunkhrmn-1205 <harunkhrmn1205@gmail.com>
2026-03-25 13:43:56 +0300

    Update api_service.py

--- app_service/api_service.py
+++ app_service/api_service.py
@@ -1,1 +1,12 @@
+from flask import Flask, request,jsonify
+from flask_cors import CORS
+import psycopg2, os
 
+
+app = Flask(__name__)
+CORS(app)
+
+
+DATABASE_URL = so.getenv(
+"DATABASE_URL",
+  "
